2371505b34aa387bb4e5b237fd58b40d0c8a1737
bhavana-kommalapati <65599680+bhavana-kommalapati@users.noreply.github.com>
2021-11-30 14:23:38 +0530

    Update RestfulWebServicesApplicationTests.java

--- src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
+++ src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -1,7 +1,5 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
-import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,11 +7,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RestfulWebServicesApplicationTests {
-
-	@Test
-	void contextLoads() {
-		Assert.assertEquals(true,true);
-	}
-
 
 }
