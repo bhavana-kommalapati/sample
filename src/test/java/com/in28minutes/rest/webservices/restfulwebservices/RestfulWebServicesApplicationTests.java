d28d977ac9d67cdc7c83d990aa24818542ab7525
bhavana-kommalapati <65599680+bhavana-kommalapati@users.noreply.github.com>
2021-11-30 13:45:44 +0530

    Update RestfulWebServicesApplicationTests.java

--- src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
+++ src/test/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,10 +12,8 @@ public class RestfulWebServicesApplicationTests {
 
 	@Test
 	void contextLoads() {
-	    Assertions.assertDoesNotThrow(this::doNotThrowException);
+		assertEquals(true,true);
 	}
 
-	private void doNotThrowException(){
-	}
 
 }
